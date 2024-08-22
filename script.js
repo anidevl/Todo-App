4f87ea44903a1102d45f45cdc41a6201fff4f9e9
anidevl <aniketwork200322@gmail.com>
2024-08-22 10:27:55 +0530

    project completed

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-let todoList=[];
+let todoList=[
+    {
+        item:'Buy Milk',
+        dueDate:'22/08/2024',
+    },
+    {
+        item:'Buy Milk',
+        dueDate:'22/08/2024',
+    }
+];
 displayItems();
 function addTodo(){
    let inputElement = document.querySelector("#todo-input");
-   let todoElement= inputElement.value;
-   todoList.push(todoElement);
+   let dateElement=document.querySelector("#todo-date");
+   let todoItem= inputElement.value;
+   let todoDate=dateElement.value;
+   todoList.push(
+    {
+        item:todoItem,
+        dueDate:todoDate,
+    }
+  );
    inputElement.value="";
+   dateElement.value="";
    displayItems();
 }
 
@@ -13,11 +30,12 @@ function displayItems(){
     let newHtml="";
     for(let i=0;i<todoList.length;i++){
         newHtml+= `
-        <div>
-         <span>${todoList[i]}</span>
-         <button onclick="todoList.splice(${i},1); displayItems();"> Delete
+       
+         <span>${todoList[i].item}</span>
+         <span>${todoList[i].dueDate}</span>
+         <button class="btn-delete" onclick="todoList.splice(${i},1); displayItems();"> Delete
          </button>
-        </div>
+       
 
         `
     }
